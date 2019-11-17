36184ba2f448af68ef84250a650ba1cede32619b
Nicolas <nicolashevia@hotmail.com>
2019-11-17 03:12:25 -0300

    Logo component full props test

--- src/components/Layout/Logo/Logo.test.js
+++ src/components/Layout/Logo/Logo.test.js
@@ -2,10 +2,31 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Logo from './Logo'
 
+const setUp = (props = {}) => {
+  const component = shallow(<Logo {...props} />)
+  return component
+}
+
 describe('Logo component', () => {
-  it('Should render without errors', () => {
-    const component = shallow(<Logo />)
+  let component
+  beforeEach(() => {
+    component = setUp()
+  })
+
+  it('Should render an image', () => {
     const wrapper = component.find('img')
+    expect(wrapper.length).toBe(1)
+  })
+
+  it('Should have className LogoHome', () => {
+    const component = shallow(<Logo isHome />)
+    const wrapper = component.find('.LogoHome')
+    expect(wrapper.length).toBe(1)
+  })
+
+  it('Should have className LogoSmall', () => {
+    const component = shallow(<Logo isHome={false} />)
+    const wrapper = component.find('.LogoSmall')
     expect(wrapper.length).toBe(1)
   })
 })
